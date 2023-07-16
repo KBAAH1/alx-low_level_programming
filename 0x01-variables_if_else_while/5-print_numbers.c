b8c867c91235c93eeb4e1e8ad15e427d5228f389
KBAAH1 <kba971124@gmail.com>
2023-07-16 11:36:40 -0700

    5-print_alphabets.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always o (Sucess)
+*/
+int main(void)
+{
+int d = 0;
+while (d <= 9)
+{
+printf("%d", d);
+++d;
+}
+printf("\n");
+return (0);
+}
